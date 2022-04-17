c06c2fd3b142f184912e60f7baae9862e66df751
Ayoubkx <ayoub@ayoub.localdomain>
2022-04-17 13:02:39 +0300

    main and run modifications

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from sqlite3 import paramstyle
 
 import mlflow
 import tempfile
@@ -49,22 +50,46 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+           
+           _= mlflow.run(
+               os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+               "main",
+               parameters={
+                   "input_artifact": config['basic_cleaning']['input_artifact'],
+                    "output_artifact": config['basic_cleaning']['output_artifact'],
+                    "output_type": config['basic_cleaning']['output_type'],
+                    "output_description": config['basic_cleaning']['output_description'],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+               },
+           ) 
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+
+            _=mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
+                "main",
+                parameters={
+                    "csv": config['data_check']['csv'],
+                    "ref": config['data_check']['ref'],
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config['etl']['min_price'],
+                    "max_price": config['etl']['max_price']
+                },
+            )
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+
+            _= mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": config["data_split"]["input"],
+                    "test_size": config['modeling']['test_size'],
+                    "random_seed": config['modeling']['random_seed'],
+                    "stratify_by": config['modeling']['stratify_by']
+                },
+            )
 
         if "train_random_forest" in active_steps:
 
@@ -76,19 +101,32 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _= mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": config["train_random_forest"]["trainval_artifact"],
+                    "rf_config": rf_config,
+                    "output_artifact": config["train_random_forest"]["output_artifact"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "val_size": config["modeling"]["val_size"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"]
+                },
+            )
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": config["test_regression_model"]["mlflow_model"],
+                    "test_dataset": config['test_regression_model']['test_dataset']
+                },
+            )
 
-            pass
+            
 
 
 if __name__ == "__main__":
