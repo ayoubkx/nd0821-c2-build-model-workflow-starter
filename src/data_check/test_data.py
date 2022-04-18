a740963a211becd5b5c99a24bcc44d6ab8fd4676
Ayoubkx <ayoub@ayoub.localdomain>
2022-04-18 13:55:45 +0300

    2nd update

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,17 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Test row count and assert that data length is reasonable
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Test price range and assert that it is with a required range
+    """
+    idx = data['price'].between(min_price, max_price)
+
+    assert np.sum(~idx) == 0
